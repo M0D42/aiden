db77cbab339cbd8a8867cfa22468bfae49c35353
M0D <167003676+M0D42@users.noreply.github.com>
2025-07-01 22:30:41 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
+
+# A very simple Flask Hello World app for you to get started with...
+
 from flask import Flask
+from aibot import aiden
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Im alive'
+
+aiden()
